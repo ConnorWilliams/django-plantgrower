358630165c49eeda54ad7c052f11130648483161
Connor Williams <connor.williams@cloudreach.com>
2017-11-21 23:08:37 -0600

    Configured overcommit and linted

--- plantgrower/models.py
+++ plantgrower/models.py
@@ -6,9 +6,11 @@ from django.db import models
 # 2. Run python manage.py makemigrations to create migrations for those changes
 # 3. Run python manage.py migrate to apply those changes to the database.
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.question_text
 
@@ -17,5 +19,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
