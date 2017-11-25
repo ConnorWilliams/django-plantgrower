507e0d397828d0bc5a5adaee7f224e067670c7cd
Connor Williams <connor.williams@cloudreach.com>
2017-11-25 17:47:33 -0600

    Installed channels

--- plant_grower/settings.py
+++ plant_grower/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'plantgrower.apps.PlantgrowerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
