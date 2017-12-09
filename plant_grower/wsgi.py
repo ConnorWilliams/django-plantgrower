b1fce0dd727c2b78c137d1dc8d9299ea3d9e17b1
Connor Williams <connor.williams@cloudreach.com>
2017-12-09 16:42:38 -0600

    Fixed wsgi.py

--- plant_grower/wsgi.py
+++ plant_grower/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/pi/plant_grower')
+sys.path.append(
+    '/home/pi/.virtualenvs/plantgrower/lib/python3.5/site-packages/'
+)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plant_grower.settings")
 
