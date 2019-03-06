459bcc0bf6a45fff21a3922c2eb9aec1cbece336
Connor Williams <connor_williams@msn.com>
2019-03-06 15:58:56 +0100

    Added tasks for outputdevice monitoring

--- plantgrower/tasks.py
+++ plantgrower/tasks.py
@@ -1,7 +1,8 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from plantgrower.models import Grow
+from plantgrower.models import Grow, OutputDevice
+from django.utils import timezone
 from plantgrower.serializers import GrowSerializer
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
@@ -24,3 +25,43 @@ def send_grow_information(path):
         )
     except Exception as e:
         logger.error('Could not send grow info - {}'.format(e))
+
+
+@shared_task
+def monitor_devices():
+    """
+    Runs on a schedule
+    Gets all devices from the DB and queues up monitor_<dvc> for each
+    """
+    output_devices = OutputDevice.objects.all()
+    for output_device in output_devices:
+        if output_device.category == 'light':
+            monitor_light.delay(output_device.light)
+
+
+@shared_task
+def monitor_light(light):
+    """
+    Sets grow_state by looking at time and veg_light_duration
+    Looks at light.user_state and light.grow_state
+    Then sends instructions to the IoT device queue to turn the light on or off
+    """
+    grow = Grow.objects.get(pk=light.grow_id)
+    if timezone.localtime(timezone.now()) > light.next_switch_time:
+        # Switch light in DB
+        light.last_switch_time = light.next_switch_time
+        light.next_switch_time += \
+            grow.dark_duration if light.grow_state else grow.light_duration
+        light.grow_state = not light.grow_state
+        light.save()
+
+    send_device_instruction(light)
+
+
+@shared_task
+def send_device_instruction(output_device):
+    # Send instruction to IoT device queue to turn device on or off
+    if output_device.user_state is not None:
+        logger.info(f"Sending instruction to turn device {output_device.user_status} from user_status")
+    else:
+        logger.info(f"Sending instruction to turn light {output_device.system_status}")
