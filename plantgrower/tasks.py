128c2e187bc8efe89e997e9ae5197c97108fc349
Connor Williams <connor_williams@msn.com>
2019-03-06 21:09:32 +0100

    Added monitor_lights scheduled task

--- plantgrower/tasks.py
+++ plantgrower/tasks.py
@@ -1,7 +1,7 @@
 # Create your tasks here
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from plantgrower.models import Grow, OutputDevice
+from plantgrower.models import Grow, OutputDevice, Light
 from django.utils import timezone
 from plantgrower.serializers import GrowSerializer
 from channels.layers import get_channel_layer
@@ -10,7 +10,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-@shared_task
+@shared_task(ignore_result=True)
 def send_grow_information(path):
     """
     Sends grow information over WebSocket to front-end for realtime updates
@@ -27,7 +27,7 @@ def send_grow_information(path):
         logger.error('Could not send grow info - {}'.format(e))
 
 
-@shared_task
+@shared_task(ignore_result=True)
 def monitor_devices():
     """
     Runs on a schedule
@@ -36,32 +36,32 @@ def monitor_devices():
     output_devices = OutputDevice.objects.all()
     for output_device in output_devices:
         if output_device.category == 'light':
-            monitor_light.delay(output_device.light)
+            monitor_light.delay(output_device.pk)
 
 
-@shared_task
-def monitor_light(light):
+@shared_task(ignore_result=True)
+def monitor_light(light_id):
     """
     Sets grow_state by looking at time and veg_light_duration
     Looks at light.user_state and light.grow_state
     Then sends instructions to the IoT device queue to turn the light on or off
     """
-    grow = Grow.objects.get(pk=light.grow_id)
+    light = Light.objects.get(pk=light_id)
     if timezone.localtime(timezone.now()) > light.next_switch_time:
         # Switch light in DB
         light.last_switch_time = light.next_switch_time
         light.next_switch_time += \
-            grow.dark_duration if light.grow_state else grow.light_duration
-        light.grow_state = not light.grow_state
+            light.grow.dark_duration if light.system_status \
+            else light.grow.light_duration
+        light.system_status = not light.system_status
         light.save()
 
-    send_device_instruction(light)
+    send_device_instruction(light.output_device)
 
 
-@shared_task
 def send_device_instruction(output_device):
     # Send instruction to IoT device queue to turn device on or off
-    if output_device.user_state is not None:
-        logger.info(f"Sending instruction to turn device {output_device.user_status} from user_status")
+    if output_device.user_status is not None:
+        logger.debug(f"Sending instruction to turn device {output_device.user_status} from user_status")
     else:
-        logger.info(f"Sending instruction to turn light {output_device.system_status}")
+        logger.debug(f"Sending instruction to turn light {output_device.system_status}")
